ae56ca2fc87fc21286fa1ac8ae8876080b777f21
Conor Smith <conormartindale@gmail.com>
2026-02-11 19:33:53 +0800

    Made new interface for Graph

--- lib/factorio/production_lines/multi_line.dart
+++ lib/factorio/production_lines/multi_line.dart
@@ -1,20 +1,46 @@
 part of '../production_line.dart';
 
 class PlanetaryBase extends ProductionLine {
+  final List<ProdLineNode> _nodes = [];
+  final List<DirectedEdge> _edges = [];
+  final Map<ItemData, double> _totalIoPerSecond = {};
+  final Set<ItemData> _allInputs = {};
+  final Set<ItemData> _allOutputs = {};
+
+  GraphUpdates _graphUpdates = GraphUpdates._();
+
+  late final List<ProdLineNode> nodes = UnmodifiableListView(_nodes);
+  late final List<DirectedEdge> edges = UnmodifiableListView(_edges);
   @override
-  Map<ItemData, double> get totalIoPerSecond => throw UnimplementedError();
+  late final Map<ItemData, double> totalIoPerSecond = UnmodifiableMapView(
+    _totalIoPerSecond,
+  );
+  @override
+  late final Set<ItemData> allInputs = UnmodifiableSetView(_allInputs);
+  @override
+  late final Set<ItemData> allOutputs = UnmodifiableSetView(_allOutputs);
+
+  GraphUpdates get updates {
+    GraphUpdates toReturn = _graphUpdates;
+    _graphUpdates = GraphUpdates._();
+    return toReturn;
+  }
 
   @override
-  void update(Map<ItemData, double> requirements) {}
+  void update(Map<ItemData, double> requirements) {
+    // TODO
+  }
 
-  @override
-  Map<ItemData, double> get ingredientsPerSecond => throw UnimplementedError();
-
-  @override
-  Map<ItemData, double> get productsPerSecond => throw UnimplementedError();
+  void addOutputNodes(Set<ItemData> itemD) {
+    // TODO
+  }
 }
 
 enum NodeType { resource, disposal, input, output, productionLine }
+
+enum ItemFlowDirection { parentToChild, childToParent }
+
+enum EdgeType { requestItems, acceptExcess }
 
 class ProdLineNode {
   final PlanetaryBase parentBase;
@@ -29,4 +55,41 @@ class ProdLineNode {
     Map<ItemData, double>? requirements,
   }) : _line = line,
        _requirements = requirements ?? {};
+
+  void makeSingleRecipe(ImmutableModuledMachineAndRecipe mmr) {
+    // TODO
+  }
+}
+
+class DirectedEdge {
+  final ItemData flow;
+  final ProdLineNode parent;
+  final ProdLineNode child;
+  double _amount;
+  ItemFlowDirection _flowDirection;
+  EdgeType _edgeType;
+
+  double get amount => _amount;
+  ItemFlowDirection get flowDirection => _flowDirection;
+  EdgeType get edgeType => _edgeType;
+
+  DirectedEdge._({
+    required this.flow,
+    required this.parent,
+    required this.child,
+    double initialAmount = 0,
+    required ItemFlowDirection flowDirection,
+    required EdgeType edgeType,
+  }) : _amount = initialAmount,
+       _flowDirection = flowDirection,
+       _edgeType = edgeType;
+}
+
+class GraphUpdates {
+  final List<ProdLineNode> newNodes = [];
+  final List<ProdLineNode> removedNodes = [];
+  final List<DirectedEdge> newEdges = [];
+  final List<DirectedEdge> removedEdges = [];
+
+  GraphUpdates._();
 }
